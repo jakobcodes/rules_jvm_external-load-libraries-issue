49ccb3eea731b68e7f5d7dda7c2005f30d835f6d
jakobcodes <jakobcodes@gmail.com>
2024-03-20 16:55:20 +0100

    Create initial project with simple WORKSPACE conf file that tries to load .so and .dylib libraries from maven central

---
+++ src/main/java/com/jakobcodes/rules_jvm_external/Main.java
@@ -0,0 +1,11 @@
+package com.jakobcodes.rules_jvm_external;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
